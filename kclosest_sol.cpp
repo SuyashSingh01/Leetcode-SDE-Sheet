0747ab39c11e08b2b297bf49af55972cb862ad74
Suyashsingh <singhsuyash045@gmail.com>
2023-05-06 08:34:52 +0530

    first commit

--- kclosest_sol.cpp
+++ kclosest_sol.cpp
@@ -1,3 +1,15 @@
+// Author : SUYASH SINGH
+/*
+Input: points = [[1,3],[-2,2]], k = 1
+Output: [[-2,2]]
+
+Explanation:
+
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+*/
 #include <iostream>
 #include <vector>
 
